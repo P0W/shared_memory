2c9c65b2667674fb00daab2fdbcf44c3c1833717
P0W <powprashant@gmail.com>
2017-10-09 17:22:13 +0530

    Sh: wq: command not found Sh: q: command not found

--- shared_mem.cpp
+++ shared_mem.cpp
@@ -42,90 +42,92 @@ _P0W!
 
 // #pragma data_seg (".myseg")
 
- // char  SharedMemoryName[12];     
- // long  long SharedMemorySize;
- // void* SharedMemoryPtr ;
- // #pragma data_seg()
- 
+// char  SharedMemoryName[12];     
+// long  long SharedMemorySize;
+// void* SharedMemoryPtr ;
+// #pragma data_seg()
+
 // void* SharedMemory( const char* shmem_name )
 // {
-    // try{
-        // boost::interprocess::shared_memory_object shm
-                // (boost::interprocess::open_only, //Open if already created
-                 // shmem_name, 
-                 // boost::interprocess::read_write);
+// try{
+// boost::interprocess::shared_memory_object shm
+// (boost::interprocess::open_only, //Open if already created
+// shmem_name, 
+// boost::interprocess::read_write);
 
-        // boost::interprocess::mapped_region region
-                // (shm, 
-                // boost::interprocess::read_write
-                // );
-        // size_t nbytes = strlen(shmem_name);      
-        // strncpy(SharedMemoryName, shmem_name, ( nbytes > 12u ) ? 12u : nbytes );
-        // SharedMemory = region.get_address();
-        // SharedMemorySize = region.get_size();
-        // return region.get_address() ;
-      // }
-    // catch(...){
-        // std::cerr << "Error Creating Shared Memory : " << shmem_name << std::endl;
-    // }
-    // return ;
+// boost::interprocess::mapped_region region
+// (shm, 
+// boost::interprocess::read_write
+// );
+// size_t nbytes = strlen(shmem_name);      
+// strncpy(SharedMemoryName, shmem_name, ( nbytes > 12u ) ? 12u : nbytes );
+// SharedMemory = region.get_address();
+// SharedMemorySize = region.get_size();
+// return region.get_address() ;
+// }
+// catch(...){
+// std::cerr << "Error Creating Shared Memory : " << shmem_name << std::endl;
+// }
+// return ;
 // }
 
 
 int CreateSharedMemory(const char* shmem_name, long long length  )
 {
-    try{
-       // remove_shmem(shmem_name);
-        boost::interprocess::shared_memory_object shm 
-		                  (boost::interprocess::create_only,
-						   shmem_name,
-		                   boost::interprocess::read_write);
+   try{
+      // remove_shmem(shmem_name);
+      boost::interprocess::shared_memory_object shm 
+         (boost::interprocess::create_only,
+         shmem_name,
+         boost::interprocess::read_write);
       //Set size
       shm.truncate(length);
       return 1;
-      }
-      
-    catch(boost::interprocess::interprocess_exception &ex){
-      std::cerr << "Error Creating Shared Memory : '" 
-                << shmem_name << "' -> "
-                << ex.what() << std::endl;  
    }
-    return 0;
+
+   catch(boost::interprocess::interprocess_exception &ex){
+      std::cerr << "Error Creating Shared Memory : '" 
+         << shmem_name << "' -> "
+         << ex.what() << std::endl;  
+   }
+   return 0;
 }
 
 int RemoveSharedMemory(const char* shmem_name )
 {   
-    /* Returns false on error. Never throws  
-       fail returning false if the shared memory does not exist,
-       the file is open or the file is still memory mapped by other processes: 
-       */
-     return   boost::interprocess::shared_memory_object::remove(shmem_name);     
+   /* Returns false on error. Never throws  
+   fail returning false if the shared memory does not exist,
+   the file is open or the file is still memory mapped by other processes: 
+   */
+   return   boost::interprocess::shared_memory_object::remove(shmem_name);     
 }
 
 int WriteSharedMemory( const char* shmem_name, const char* shmem )
 {
-    try{
-        boost::interprocess::shared_memory_object shm
-                        (
-                        boost::interprocess::open_only, //Open if already created
-                        shmem_name, 
-                        boost::interprocess::read_write
-                        );
+   try{
+      boost::interprocess::shared_memory_object shm
+         (
+         boost::interprocess::open_only, //Open if already created
+         shmem_name, 
+         boost::interprocess::read_write
+         );
 
       //Map the whole shared memory in this process
-        boost::interprocess::mapped_region region
-                        (
-                        shm, 
-                        boost::interprocess::read_write
-                        );
-     
-        std::memcpy(region.get_address(), shmem, region.get_size());
+      boost::interprocess::mapped_region region
+         (
+         shm, 
+         boost::interprocess::read_write
+         );
+
+      std::memcpy(region.get_address(), shmem, region.get_size());
       return 1;
-      }
-      catch(boost::interprocess::interprocess_exception &ex){
+   }
+   catch(boost::interprocess::interprocess_exception &ex){
+#if 0
       std::cerr << "Error Writing to Shared Memory : '" 
-                << shmem_name << "' -> "
-                << ex.what() << std::endl;
+         << shmem_name << "' -> "
+         << ex.what() << std::endl;
+#endif
    }
    return 0;
 }
@@ -133,32 +135,34 @@ int WriteSharedMemory( const char* shmem_name, const char* shmem )
 
 int ReadSharedMemory( const char* shmem_name, char *shmem )
 {
-      //Open already created shared memory object.
-	 // Same process might not be calling it, so create new object
-    try{
-        boost::interprocess::shared_memory_object shm 
-                       (
-                        boost::interprocess::open_only, 
-                        shmem_name, 
-                        boost::interprocess::read_only
-                        );
+   //Open already created shared memory object.
+   // Same process might not be calling it, so create new object
+   try{
+      boost::interprocess::shared_memory_object shm 
+         (
+         boost::interprocess::open_only, 
+         shmem_name, 
+         boost::interprocess::read_only
+         );
 
       //Map the whole shared memory in this process
-        boost::interprocess::mapped_region region
-                        (
-                        shm, 
-                        boost::interprocess::read_only
-                        );
-     
+      boost::interprocess::mapped_region region
+         (
+         shm, 
+         boost::interprocess::read_only
+         );
+
       //CAUTION : No check is performed
-	  std::memcpy( shmem, reinterpret_cast<char *>(region.get_address()), region.get_size() ); 
+      std::memcpy( shmem, reinterpret_cast<char *>(region.get_address()), region.get_size() ); 
       return 1;
 
-      }
-      catch(boost::interprocess::interprocess_exception &ex){
+   }
+   catch(boost::interprocess::interprocess_exception &ex){
+#if 0
       std::cerr << "Error Reading to Shared Memory : '" 
-                << shmem_name << "' -> "
-                << ex.what() << std::endl;
+         << shmem_name << "' -> "
+         << ex.what() << std::endl;
+#endif
    }
    return 0;
 }
@@ -166,30 +170,32 @@ int ReadSharedMemory( const char* shmem_name, char *shmem )
 
 void* GetSharedMemoryRegion( const char* shmem_name )
 {
-    try{
-        boost::interprocess::shared_memory_object shm 
-                       (
-                        boost::interprocess::open_only, 
-                        shmem_name, 
-                        boost::interprocess::read_only
-                        );
+   try{
+      boost::interprocess::shared_memory_object shm 
+         (
+         boost::interprocess::open_only, 
+         shmem_name, 
+         boost::interprocess::read_only
+         );
 
       //Map the whole shared memory in this process
-        boost::interprocess::mapped_region region
-                        (
-                        shm, 
-                        boost::interprocess::read_only
-                        );
-     
-      //CAUTION : No check is performed
-	  return region.get_address() ;
-      }
-      
-    catch(boost::interprocess::interprocess_exception &ex){
-      std::cerr << "Error Reading to Shared Memory : '" 
-                << shmem_name << "' -> "
-                << ex.what() << std::endl;
-    }
+      boost::interprocess::mapped_region region
+         (
+         shm, 
+         boost::interprocess::read_only
+         );
 
-    return NULL ;
+      //CAUTION : No check is performed
+      return region.get_address() ;
+   }
+
+   catch(boost::interprocess::interprocess_exception &ex){
+#if 0
+      std::cerr << "Error Reading to Shared Memory : '" 
+         << shmem_name << "' -> "
+         << ex.what() << std::endl;
+#endif
+   }
+
+   return NULL ;
 }
--- shared_mem.h
+++ shared_mem.h
@@ -18,36 +18,36 @@ Ref: http://www.boost.org/
 */
 
 #ifdef __cplusplus
-    #include<iostream>
-    #include<cstring>
-    #include <boost/interprocess/shared_memory_object.hpp>
-    #include <boost/interprocess/mapped_region.hpp>
-    extern "C" {
+#include<iostream>
+#include<cstring>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+extern "C" {
 #endif
 
 #ifndef _SHMEM_H_
-    #define _SHMEM_H_
-    #ifndef USE_DLL
-        #ifndef __cplusplus
-            #error Must use C++ to compile this DLL
-        #else
-            #define DLL_EXPORT_TYPE __declspec( dllexport )
-         #endif
-    #else
-        #define DLL_EXPORT_TYPE __declspec( dllimport )
-    #endif
-    
-// static void* SharedMemoryPtr;             // Shared Memory Address
-// DLL_EXPORT_TYPE extern  char SharedMemoryName[12];     //  Shared Memory Name
-// DLL_EXPORT_TYPE extern  long long SharedMemorySize;   //   Shared Memory Size
-// DLL_EXPORT_TYPE  void SetSharedMemory    ( const char*  );
-DLL_EXPORT_TYPE  int CreateSharedMemory ( const char*, long long );
-DLL_EXPORT_TYPE  int RemoveSharedMemory ( const char*  );
-DLL_EXPORT_TYPE  int WriteSharedMemory  ( const char*, const char*  ) ;
-DLL_EXPORT_TYPE  int ReadSharedMemory   ( const char*, char* );
-DLL_EXPORT_TYPE  void* GetSharedMemoryRegion ( const char*  );
+#define _SHMEM_H_
+#ifndef USE_DLL
+#ifndef __cplusplus
+#error Must use C++ to compile this DLL
+#else
+#define DLL_EXPORT_TYPE __declspec( dllexport )
+#endif
+#else
+#define DLL_EXPORT_TYPE __declspec( dllimport )
+#endif
+
+   // static void* SharedMemoryPtr;             // Shared Memory Address
+   // DLL_EXPORT_TYPE extern  char SharedMemoryName[12];     //  Shared Memory Name
+   // DLL_EXPORT_TYPE extern  long long SharedMemorySize;   //   Shared Memory Size
+   // DLL_EXPORT_TYPE  void SetSharedMemory    ( const char*  );
+   DLL_EXPORT_TYPE  int CreateSharedMemory ( const char*, long long );
+   DLL_EXPORT_TYPE  int RemoveSharedMemory ( const char*  );
+   DLL_EXPORT_TYPE  int WriteSharedMemory  ( const char*, const char*  ) ;
+   DLL_EXPORT_TYPE  int ReadSharedMemory   ( const char*, char* );
+   DLL_EXPORT_TYPE  void* GetSharedMemoryRegion ( const char*  );
 #endif
 
 #ifdef __cplusplus
-    }
+}
 #endif
